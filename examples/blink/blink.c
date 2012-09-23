85779ac90398295f13f3c8da575d76213a7f9980
Simon Schubert <2@0x2c.org>
2012-09-24 01:59:32 +0200

    update blink example

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -1,28 +1,16 @@
 #include <mchck.h>
-#include "stm32l1xx_gpio.h"
-#include "stm32l1xx_rcc.h"
 
 int
 main(void)
 {
-	/* The LED is on PC13 */
-
-        static GPIO_InitTypeDef pininit = {
-		.GPIO_Pin = 1 << 13,
-		.GPIO_Mode = GPIO_Mode_OUT,
-		.GPIO_Speed = GPIO_Speed_400KHz,
-		.GPIO_OType = GPIO_OType_PP,
-		.GPIO_PuPd = GPIO_PuPd_NOPULL
-	};
-
-	/* Enable GPIOC clock */
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	/* Configure pin as GPIO */
+	PORTC_PCR0 = PORT_PCR_MUX(1) | (1 << PORT_PCR_DSE_SHIFT);
 
 	/* Configure pin as output */
-	GPIO_Init(GPIOC, &pininit);
+	GPIOC_PDDR = 1 << 0;
 
 	for (;;) {
-		GPIO_ToggleBits(GPIOC, 1 << 13);
+		GPIOC_PTOR = 1 << 0;
 		for (volatile int i = 1000000; i > 0; --i);
 	}
 }
