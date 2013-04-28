8b6f0030f748a985e1d77dd9e6cf63ea1d6a4757
Simon Schubert <2@0x2c.org>
2013-04-28 12:10:26 +0200

    usb-dfu: attach to vhci

--- bootloader/usb-dfu/usb.c
+++ bootloader/usb-dfu/usb.c
@@ -4,393 +4,10 @@
 #include <string.h>
 #include <wchar.h>
 
-/**
- * Note: bitfields ahead.
- * GCC fills the fields lsb-to-msb on little endian.
- */
-
-#define __packed __attribute__((__packed__))
-
-/* From FreeBSD: compile-time asserts */
-#define CTASSERT(x)             _CTASSERT(x, __LINE__)
-#define _CTASSERT(x, y)         __CTASSERT(x, y)
-#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
-
-#define CTASSERT_SIZE_BYTE(t, s)     CTASSERT(sizeof(t) == (s))
-#define CTASSERT_SIZE_BIT(t, s)     CTASSERT(sizeof(t) * 8 == (s))
-
-
-/**
- * USB descriptors
- */
-
-enum usb_desc_type {
-	USB_DESC_DEV = 1,
-	USB_DESC_CONFIG = 2,
-	USB_DESC_STRING = 3,
-	USB_DESC_IFACE = 4,
-	USB_DESC_EP = 5,
-	USB_DESC_DEVQUAL = 6,
-	USB_DESC_OTHERSPEED = 7,
-	USB_DESC_POWER = 8
-};
-
-enum usb_dev_class {
-	USB_DEV_CLASS_SEE_IFACE = 0,
-	USB_DEV_CLASS_VENDOR = 0xff
-};
-
-enum usb_dev_subclass {
-	USB_DEV_SUBCLASS_SEE_IFACE = 0,
-	USB_DEV_SUBCLASS_VENDOR = 0xff
-};
-
-enum usb_dev_proto {
-	USB_DEV_PROTO_SEE_IFACE = 0,
-	USB_DEV_PROTO_VENDOR = 0xff
-};
-
-union usb_bcd_t {
-	struct {
-		uint8_t sub : 4;
-		uint8_t min : 4;
-		uint16_t maj : 8;
-	};
-	uint16_t bcd;
-};
-CTASSERT_SIZE_BYTE(union usb_bcd_t, 2);
-
-struct usb_desc_dev_t {
-	uint8_t desc_length;
-	enum usb_desc_type desc_type : 8; /* = USB_DESC_DEV */
-	union usb_bcd_t usbver;	     /* = 0x0200 */
-	enum usb_dev_class devclass : 8;
-	enum usb_dev_subclass devsubclass : 8;
-	enum usb_dev_proto : 8;
-	uint8_t ep0_maxsize;
-	uint16_t vid;
-	uint16_t pid;
-	union usb_bcd_t devver;
-	uint8_t manuf_strdesc;
-	uint8_t prod_strdesc;
-	uint8_t serial_strdesc;
-	uint8_t numconfig;
-} __packed;
-CTASSERT_SIZE_BYTE(struct usb_desc_dev_t, 18);
-
-struct usb_desc_config_t {
-	uint8_t desc_length;
-	enum usb_desc_type desc_type : 8; /* = USB_DESC_CONFIG */
-	uint16_t total_length;	     /* size of config, iface, ep */
-	uint8_t num_ifaces;
-	uint8_t config_val;
-	uint8_t config_strdesc;
-	struct {
-		uint8_t _rsvd0 : 5;
-		uint8_t remote_wakeup : 1;
-		uint8_t self_powered : 1;
-		uint8_t one : 1; /* = 1 for historical reasons */
-	};
-	uint8_t maxpower;	/* units of 2mA */
-} __packed;
-CTASSERT_SIZE_BYTE(struct usb_desc_config_t, 9);
-
-enum usb_iface_class {
-	USB_IFACE_CLASS_SEE_IFACE = 0,
-	USB_IFACE_CLASS_VENDOR = 0xff
-};
-
-enum usb_iface_subclass {
-	USB_IFACE_SUBCLASS_SEE_IFACE = 0,
-	USB_IFACE_SUBCLASS_VENDOR = 0xff
-};
-
-enum usb_iface_proto {
-	USB_IFACE_PROTO_SEE_IFACE = 0,
-	USB_IFACE_PROTO_VENDOR = 0xff
-};
-
-struct usb_desc_iface_t {
-	uint8_t desc_length;
-	enum usb_desc_type desc_type : 8; /* = USB_DESC_IFACE */
-	uint8_t iface_num;
-	uint8_t alternate;
-	uint8_t num_ep;
-	enum usb_iface_class : 8;
-	enum usb_iface_subclass : 8;
-	enum usb_iface_proto : 8;
-	uint8_t iface_strdesc;
-} __packed;
-CTASSERT_SIZE_BYTE(struct usb_desc_iface_t, 9);
-
-struct usb_desc_ep_t {
-	uint8_t desc_length;
-	enum usb_desc_type desc_type : 8; /* = USB_DESC_EP */
-	union {
-		struct {
-			uint8_t ep_num : 4;
-			uint8_t _rsvd0 : 3;
-			uint8_t in : 1;
-		};
-		uint8_t addr;
-	};
-	struct {
-		enum usb_ep_type {
-			USB_EP_CONTROL = 0,
-			USB_EP_ISO = 1,
-			USB_EP_BULK = 2,
-			USB_EP_INTR = 3
-		} type : 2;
-		enum usb_ep_iso_synctype {
-			USB_EP_ISO_NOSYNC = 0,
-			USB_EP_ISO_ASYNC = 1,
-			USB_EP_ISO_ADAPTIVE = 2,
-			USB_EP_ISO_SYNC = 3
-		} sync_type : 2;
-		enum usb_ep_iso_usagetype {
-			USB_EP_ISO_DATA = 0,
-			USB_EP_ISO_FEEDBACK = 1,
-			USB_EP_ISO_IMPLICIT = 2
-		} usage_type : 2;
-		uint8_t _rsvd1 : 2;
-	} __packed;
-	struct {
-		uint16_t maxsize : 11;
-		uint16_t _rsvd2 : 5;
-	};
-	uint8_t interval;
-} __packed;
-CTASSERT_SIZE_BYTE(struct usb_desc_ep_t, 7);
-
-struct usb_desc_string_t {
-	uint8_t desc_length;
-	enum usb_desc_type desc_type : 8; /* = USB_DESC_STRING */
-	wchar_t string[];
-};
-
-
-/**
- * USB request data structures.
- */
-
-enum usb_tok_pid {
-	USB_PID_TIMEOUT = 0,
-	USB_PID_OUT = 1,
-	USB_PID_ACK = 2,
-	USB_PID_DATA0 = 3,
-	USB_PID_IN = 9,
-	USB_PID_NAK = 10,
-	USB_PID_DATA1 = 11,
-	USB_PID_SETUP = 13,
-	USB_PID_STALL = 14,
-	USB_PID_DATAERR = 15
-};
-
-struct usb_ctrl_req_t {
-	union /* reqtype and request & u16 */ {
-		struct /* reqtype and request */ {
-			union /* reqtype in bitfield & u8 */ {
-				struct /* reqtype */ {
-					enum usb_ctrl_req_recp {
-						USB_CTRL_REQ_DEV = 0,
-						USB_CTRL_REQ_IFACE = 1,
-						USB_CTRL_REQ_EP = 2,
-						USB_CTRL_REQ_OTHER = 3
-					} recp : 5;
-					enum usb_ctrl_req_type {
-						USB_CTRL_REQ_STD = 0,
-						USB_CTRL_REQ_CLASS = 1,
-						USB_CTRL_REQ_VENDOR = 2
-					} type : 2;
-					enum usb_ctrl_req_dir {
-						USB_CTRL_REQ_OUT = 0,
-						USB_CTRL_REQ_IN = 1
-					} in : 1;
-				} __packed;
-				uint8_t reqtype;
-			}; /* union */
-			enum usb_ctrl_req_code {
-				USB_CTRL_REQ_GET_STATUS = 0,
-				USB_CTRL_REQ_CLEAR_FEATURE = 1,
-				USB_CTRL_REQ_SET_FEATURE = 3,
-				USB_CTRL_REQ_SET_ADDRESS = 5,
-				USB_CTRL_REQ_GET_DESCRIPTOR = 6,
-				USB_CTRL_REQ_SET_DESCRIPTOR = 7,
-				USB_CTRL_REQ_GET_CONFIGURATION = 8,
-				USB_CTRL_REQ_SET_CONFIGURATION = 9,
-				USB_CTRL_REQ_GET_INTERFACE = 10,
-				USB_CTRL_REQ_SET_INTERFACE = 11,
-				USB_CTRL_REQ_SYNC_FRAME = 12
-			} request : 8;
-		} __packed; /* struct */
-		uint16_t type_and_req;
-	}; /* union */
-	uint16_t value;
-	uint16_t index;
-	uint16_t length;
-} __packed;
-CTASSERT_SIZE_BYTE(struct usb_ctrl_req_t, 8);
-
-#define USB_CTRL_REQ_DIR_SHIFT 0
-#define USB_CTRL_REQ_TYPE_SHIFT 1
-#define USB_CTRL_REQ_RECP_SHIFT 3
-#define USB_CTRL_REQ_CODE_SHIFT 8
-#define USB_CTRL_REQ(req_inout, req_type, req_code)			\
-	(uint16_t)							\
-	((USB_CTRL_REQ_##req_inout << USB_CTRL_REQ_DIR_SHIFT)		\
-	 |(USB_CTRL_REQ_##req_type << USB_CTRL_REQ_TYPE_SHIFT)		\
-	 |(USB_CTRL_REQ_##req_code << USB_CTRL_REQ_CODE_SHIFT))
-
-/**
- * status replies for GET_STATUS.
- */
-
-struct usb_ctrl_req_status_dev_t {
-	uint16_t self_powered : 1;
-	uint16_t remote_wakeup : 1;
-	uint16_t _rsvd : 14;
-};
-CTASSERT_SIZE_BIT(struct usb_ctrl_req_status_dev_t, 16);
-
-struct usb_ctrl_req_status_iface_t {
-	uint16_t _rsvd;
-};
-CTASSERT_SIZE_BIT(struct usb_ctrl_req_status_iface_t, 16);
-
-struct usb_ctrl_req_status_ep_t {
-	uint16_t halt : 1;
-	uint16_t _rsvd : 15;
-};
-CTASSERT_SIZE_BIT(struct usb_ctrl_req_status_ep_t, 16);
-
-/**
- * Descriptor type (in req->value) for GET_DESCRIPTOR.
- */
-struct usb_ctrl_req_desc_t {
-	uint8_t idx;
-	enum usb_desc_type type : 8;
-} __packed;
-CTASSERT_SIZE_BIT(struct usb_ctrl_req_desc_t, 16);
-
-/**
- * Feature selector (in req->value) for CLEAR_FEATURE.
- */
-enum usb_ctrl_req_feature {
-	USB_CTRL_REQ_FEAT_EP_HALT = 0,
-	USB_CTRL_REQ_FEAT_DEV_REMOTE_WKUP = 1,
-	USB_CTRL_REQ_FEAT_TEST_MODE = 2
-};
-
-
-/**
- * Internal driver structures
- */
-
-/**
- * USB state machine
- * =================
- *
- * Device configuration states:
- *
- * Attached <-> Powered
- * Powered -(reset)-> Default
- * Default -(SET_ADDRESS)-> Address
- * Address -(SET_CONFIGURATION)-> Configured
- * Configured -(SET_CONFIGURATION 0)-> Address
- * Address -(SET_ADDRESS 0)-> Default
- * [Default, Configured, Address] -(reset)-> Default
- */
-
-
-/**
- * Kinetis USB driver notes:
- * We need to manually maintain the DATA0/1 toggling for the SIE.
- * SETUP transactions always start with a DATA0.
- *
- * The SIE internally uses pingpong (double) buffering, which is
- * easily confused with DATA0/DATA1 toggling, and I think even the
- * Freescale docs confuse the two notions.  When BD->DTS is set,
- * BD->DATA01 will be used to verify/discard incoming DATAx and it
- * will set the DATAx PID for outgoing tokens.  This is not described
- * as such in the Freescale Kinetis docs, but the Microchip PIC32 OTG
- * docs are more clear on this;  it seems that both Freescale and
- * Microchip use different versions of the same USB OTG IP core.
- *
- * http://ww1.microchip.com/downloads/en/DeviceDoc/61126F.pdf
- *
- * Clear CTL->TOKEN_BUSY after SETUP tokens.
- */
-
-#define EP0_BUFSIZE 64
-
-enum usb_ep_pingpong {
-	USB_EP_PINGPONG_EVEN = 0,
-	USB_EP_PINGPONG_ODD = 1
-};
-
-enum usb_ep_dir {
-	USB_EP_RX = 0,
-	USB_EP_TX = 1
-};
-
-enum usb_data01 {
-	USB_DATA01_DATA0 = 0,
-	USB_DATA01_DATA1 = 1
-};
-
-enum usbd_dev_state {
-	USBD_STATE_DISABLED = 0,
-	USBD_STATE_DEFAULT,
-	USBD_STATE_SETTING_ADDRESS,
-	USBD_STATE_ADDRESS,
-	USBD_STATE_CONFIGURED
-};
-
-typedef void (*ep_callback_t)(void *buf, ssize_t len, void *data);
-
-
-struct usbd_ep_pipe_state_t {
-	enum usb_ep_pingpong pingpong; /* next desc to use */
-	enum usb_data01 data01;
-	size_t transfer_size;
-	size_t pos;
-	uint8_t *data_buf;
-	int short_transfer;
-	ep_callback_t callback;
-	void *callback_data;
-	size_t ep_maxsize;
-};
-
-struct usbd_ep_state_t {
-	union {
-		struct usbd_ep_pipe_state_t pipe[2];
-		struct {
-			struct usbd_ep_pipe_state_t rx;
-			struct usbd_ep_pipe_state_t tx;
-		};
-	};
-};
-
-struct usbd_t {
-	struct USB_BD_t *bdt;
-	enum usbd_dev_state state;
-	enum usbd_ctrl_state {
-		USBD_CTRL_STATE_IDLE,
-		USBD_CTRL_STATE_DATA,
-		USBD_CTRL_STATE_STATUS
-	} ctrl_state;
-	enum usb_ctrl_req_dir ctrl_dir;
-	int address;
-	int config;
-	struct usbd_ep_state_t ep0_state;
-	uint8_t ep0_buf[EP0_BUFSIZE][2];
-};
+#include "usb.h"
 
 
 struct usbd_t usb;
-
-
-#include "vusb.c"
 
 
 /**
@@ -398,7 +15,7 @@ struct usbd_t usb;
  * more to go.
  */
 /* Defaults to EP0 for now */
-static int
+int
 usb_tx_next(void)
 {
 	struct usbd_ep_pipe_state_t *s = &usb.ep0_state.tx;
@@ -479,7 +96,7 @@ usb_tx_cp(void *buf, size_t len)
  */
 /* Defaults to EP0 for now */
 /* XXX pass usb_stat to validate pingpong */
-static int
+int
 usb_rx_next(void)
 {
 	struct usbd_ep_pipe_state_t *s = &usb.ep0_state.rx;
@@ -578,7 +195,7 @@ usb_rx(void *buf, size_t len, ep_callback_t cb, void *cb_data)
  * this to save space.
  */
 
-static int
+int
 usb_handle_control(struct usb_ctrl_req_t *req)
 {
 	uint16_t zero16 = 0;
@@ -650,7 +267,7 @@ err:
 	return (-1);
 }
 
-static void
+void
 usb_setup_control(void)
 {
 	void *buf = usb.ep0_buf[usb.ep0_state.rx.pingpong];
@@ -661,12 +278,15 @@ usb_setup_control(void)
 }
 
 void
-usb_handle_control_ep(usb_xfer_info_t stat)
+usb_handle_control_ep(struct usb_xfer_info *stat)
 {
 	struct usb_ctrl_req_t *req;
 	int r;
 
 	switch (usb_get_xfer_pid(stat)) {
+	default:
+		/* unknown PID? */
+		break;
 	case USB_PID_SETUP:
 		usb_clear_transfers();
 
--- bootloader/usb-dfu/vusb.c
+++ bootloader/usb-dfu/vusb.c
@@ -1,26 +1,27 @@
-typedef int usb_xfer_info_t;    /* XXX */
+#include <sys/socket.h>
+
+#include <err.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "usb.h"
+
+void *
+usb_get_xfer_data(struct usb_xfer_info *i)
+{
+}
+
+enum usb_tok_pid
+usb_get_xfer_pid(struct usb_xfer_info *i)
+{
+}
 
 void
-usb_enable(void)
-{
-}
-
-static void *
-usb_get_xfer_data(usb_xfer_info_t i)
-{
-}
-
-static enum usb_tok_pid
-usb_get_xfer_pid(usb_xfer_info_t i)
-{
-}
-
-static void
 usb_enable_xfers(void)
 {
 }
 
-static void
+void
 usb_set_addr(int addr)
 {
 }
@@ -30,22 +31,102 @@ usb_ep_stall(int ep)
 {
 }
 
-static void
+void
 usb_clear_transfers(void)
 {
 }
 
-static inline size_t
+inline size_t
 usb_ep_get_transfer_size(int ep, enum usb_ep_dir dir, enum usb_ep_pingpong pingpong)
 {
 }
 
-static void
+void
 usb_tx_queue_next(struct usbd_ep_pipe_state_t *s)
 {
 }
 
-static void
+void
 usb_rx_queue_next(struct usbd_ep_pipe_state_t *s)
 {
+}
+
+
+static int vusb_sock;
+
+static void
+vusb_attach(void)
+{
+        /**
+         * Interfacing to usbip-host/vhci_hcd:
+         *
+         * 1. find free roothub port
+         * 2. create socket to pass to the kernel
+         * 3. attach
+         */
+
+        /* (1) find free roothub port */
+        FILE *statusf = fopen("/sys/devices/platform/vhci_hcd/status", "r");
+
+        if (statusf == NULL)
+                errx(1, "usbip-host module not loaded?");
+
+        int nextport = -1;
+
+        /* skip header */
+        fscanf(statusf, "%*[^\n]\n");
+        while (!feof(statusf)) {
+                int portid, status;
+                if (fscanf(statusf, "%d %d %*[^\n]\n", &portid, &status) != 2)
+                        warnx("odd vhci status line");
+
+                if (status == 4) {
+                        nextport = portid;
+                        break;
+                }
+        }
+        fclose(statusf);
+
+        if (nextport < 0)
+                errx(1, "no free port found");
+
+        /* (2) create socket */
+        int sockp[2];
+        if (socketpair(PF_UNIX, SOCK_STREAM, 0, sockp) < 0)
+                err(1, "cannot create socket");
+
+        /* (3) pass to kernel */
+        /* XXX needs root priv */
+        FILE *attachf = fopen("/sys/devices/platform/vhci_hcd/attach", "w");
+        if (attachf == NULL)
+                err(1, "cannot open attach to vhci");
+        if (fprintf(attachf, "%u %u %u %u",
+                    nextport,
+                    sockp[0],
+                    nextport + 1,   /* devid */
+                    2               /* USB_SPEED_FULL */
+                    ) < 0 ||
+            fclose(attachf) < 0)
+                err(1, "cannot attach to vhci");
+
+        /* belongs to the kernel now */
+        close(sockp[0]);
+
+        vusb_sock = sockp[1];
+}
+
+static void
+vusb_rcv(void)
+{
+        
+}
+
+
+int
+main(void)
+{
+        vusb_attach();
+        for (;;) {
+                vusb_rcv();
+        }
 }
