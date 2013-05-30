c86b1cb891f4751dffe05e258b66bb5a30b4fcad
Simon Schubert <2@0x2c.org>
2013-05-30 15:24:44 +0200

    toolchain: add CTASSERT macros to mchck.h

--- toolchain/include/mchck.h
+++ toolchain/include/mchck.h
@@ -12,7 +12,18 @@
 #include <stdint.h>
 
 
+//#include <uchar.h>
+typedef __CHAR16_TYPE__ char16_t;
+
 #define __packed __attribute__((__packed__))
+
+/* From FreeBSD: compile-time asserts */
+#define CTASSERT(x)             _CTASSERT(x, __LINE__)
+#define _CTASSERT(x, y)         __CTASSERT(x, y)
+#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+
+#define CTASSERT_SIZE_BYTE(t, s)     CTASSERT(sizeof(t) == (s))
+#define CTASSERT_SIZE_BIT(t, s)     CTASSERT(sizeof(t) * 8 == (s))
 
 
 /* From CMSIS: */
