339a9f8253e35ca48d60a9e82db2ae61e8f968f0
Simon Schubert <2@0x2c.org>
2013-05-30 23:23:50 +0200

    usb-dfu: more nice bitfields

--- bootloader/usb-dfu/main-kinetis.c
+++ bootloader/usb-dfu/main-kinetis.c
@@ -22,32 +22,38 @@ main(void)
 //        MCG.dmx32 = 1;           /* mcg at 24MHz */
 //        SIM.clkdiv2.usbfrac = 1; /* usb = 2x core */
         OSC_CR = OSC_CR_SC16P_MASK;
-        MCG.range0 = MCG_RANGE_VERYHIGH;
-        MCG.erefs0 = MCG_EREF_OSC;
-        /* MCG.frdiv = 4; */
-        /* MCG.clks = MCG_CLKS_EXTERNAL; */
-        /* MCG.irefs = 0; */
-        MCG_C1 = MCG_C1_CLKS(2) | MCG_C1_FRDIV(4);
+        MCG.c2.raw = ((struct MCG_C2_t){
+                        .range0 = MCG_RANGE_VERYHIGH,
+                                .erefs0 = MCG_EREF_OSC
+                                }).raw;
+        MCG.c1.raw = ((struct MCG_C1_t){
+                        .clks = MCG_CLKS_EXTERNAL,
+                                .frdiv = 4,
+                                .irefs = 0
+                                }).raw;
 
-        while (!MCG.oscinit0)
+        while (!MCG.s.oscinit0)
                 /* NOTHING */;
-        while (MCG.clkst != MCG_CLKST_EXTERNAL)
-                /* NOTHING */;
-
-        MCG.prdiv0 = 7;
-        MCG.pllclken0 = 1;
-        MCG.c6 = 0;
-        MCG.vdiv0 = 0;
-        MCG.plls = 1;
-
-        while (!MCG.pllst)
-                /* NOTHING */;
-        while (!MCG.lock0)
+        while (MCG.s.clkst != MCG_CLKST_EXTERNAL)
                 /* NOTHING */;
 
-        MCG.clks = MCG_CLKS_FLLPLL;
+        MCG.c5.raw = ((struct MCG_C5_t){
+                        .prdiv0 = 7,
+                                .pllclken0 = 1
+                                }).raw;
+        MCG.c6.raw = ((struct MCG_C6_t){
+                        .vdiv0 = 0,
+                        .plls = 1
+                                }).raw;
 
-        while (MCG.clkst != MCG_CLKST_PLL)
+        while (!MCG.s.pllst)
+                /* NOTHING */;
+        while (!MCG.s.lock0)
+                /* NOTHING */;
+
+        MCG.c1.clks = MCG_CLKS_FLLPLL;
+
+        while (MCG.s.clkst != MCG_CLKST_PLL)
                 /* NOTHING */;
 
         SIM.sopt2.pllfllsel = SIM_PLLFLLSEL_PLL;
