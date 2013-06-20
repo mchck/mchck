f899c29514d0c3db09179c5d90e739f1301ce0e0
Simon Schubert <2@0x2c.org>
2013-06-20 14:40:53 +0200

    programmer/flash: add --reset

--- programmer/flash.rb
+++ programmer/flash.rb
@@ -2,25 +2,32 @@ $: << File.realpath('..', __FILE__)
 
 require 'kinetis'
 require 'backend-driver'
+require 'optparse'
 
-if $0 == __FILE__
-  $stderr.puts "Attaching debugger..."
-  adiv5 = Adiv5.new(BackendDriver.from_string(ARGV[0]))
-  k = Kinetis.new(adiv5)
-  $stderr.puts "done."
+force_reset = false
 
-  firmware = File.read(ARGV[1], :mode => 'rb')
-  address = Integer(ARGV[2])
-
-  $stderr.puts "Programming %d bytes of firmware to address %#x..." % [firmware.bytesize, address]
-  k.halt_core!
-  k.program(address, firmware) do |address, i, total|
-    $stderr.puts "programming %#x, %d of %d" % [address, i, total]
+OptionParser.new do |opts|
+  opts.on("--reset", "Force system into reset") do |r|
+    force_reset = true
   end
-  $stderr.puts "done."
-  $stderr.puts "resetting..."
-  k.reset_system!
-  k.disable_debug!
-  k.continue!
-  $stderr.puts "done."
+end.parse!
+
+$stderr.puts "Attaching debugger..."
+adiv5 = Adiv5.new(BackendDriver.from_string(ARGV[0]))
+k = Kinetis.new(adiv5, force_reset)
+$stderr.puts "done."
+
+firmware = File.read(ARGV[1], :mode => 'rb')
+address = Integer(ARGV[2])
+
+$stderr.puts "Programming %d bytes of firmware to address %#x..." % [firmware.bytesize, address]
+k.halt_core!
+k.program(address, firmware) do |address, i, total|
+  $stderr.puts "programming %#x, %d of %d" % [address, i, total]
 end
+$stderr.puts "done."
+$stderr.puts "resetting..."
+k.reset_system!
+k.disable_debug!
+k.continue!
+$stderr.puts "done."
