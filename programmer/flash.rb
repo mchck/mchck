4270cf40418ec61d4fcd8c7e622928f9f0c559ca
Simon Schubert <2@0x2c.org>
2013-06-20 14:43:51 +0200

    programmer/flash: actually always hold IC in reset

--- programmer/flash.rb
+++ programmer/flash.rb
@@ -2,19 +2,10 @@ $: << File.realpath('..', __FILE__)
 
 require 'kinetis'
 require 'backend-driver'
-require 'optparse'
-
-force_reset = false
-
-OptionParser.new do |opts|
-  opts.on("--reset", "Force system into reset") do |r|
-    force_reset = true
-  end
-end.parse!
 
 $stderr.puts "Attaching debugger..."
 adiv5 = Adiv5.new(BackendDriver.from_string(ARGV[0]))
-k = Kinetis.new(adiv5, force_reset)
+k = Kinetis.new(adiv5, true)
 $stderr.puts "done."
 
 firmware = File.read(ARGV[1], :mode => 'rb')
