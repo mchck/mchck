13d13f0e8dcefbb2e971c5750bfadd9723499ff1
Simon Schubert <2@0x2c.org>
2013-07-31 19:32:36 +0200

    usb: add cdc-acm

---
+++ toolchain/lib/usb/cdc-acm.c
@@ -0,0 +1,90 @@
+#include <mchck.h>
+
+#include <usb/usb.h>
+
+#include "cdc-acm.h"
+
+
+static void
+cdc_rx_done(void *buf, ssize_t len, void *data)
+{
+        struct cdc_ctx *ctx = data;
+
+        if (len <= 0)
+                goto queue;
+
+        ctx->data_ready_cb(buf, len);
+queue:
+        usb_rx(ctx->rx_pipe, ctx->inbuf, sizeof(ctx->inbuf), cdc_rx_done, ctx);
+}
+
+static void
+cdc_tx_done(void *buf, ssize_t len, void *data)
+{
+        struct cdc_ctx *ctx = data;
+
+        if (len <= 0)
+                goto queue;
+
+        crit_enter();
+        ctx->out_sent += len;
+        memcpy(ctx->outbuf, &ctx->outbuf[ctx->out_sent], ctx->out_pos - ctx->out_sent);
+        ctx->out_pos -= ctx->out_sent;
+        ctx->out_sent = 0;
+        ctx->out_queued = 0;
+        crit_exit();
+        if (ctx->data_sent_cb != NULL)
+                ctx->data_sent_cb(sizeof(ctx->outbuf) - ctx->out_pos);
+queue:
+        crit_enter();
+        if (ctx->out_pos > 0 && !ctx->out_queued) {
+                ctx->out_queued = 1;
+                usb_tx(ctx->tx_pipe, ctx->outbuf, ctx->out_pos, CDC_TX_SIZE, cdc_tx_done, ctx);
+        }
+        crit_exit();
+}
+
+ssize_t
+cdc_write(const char *buf, size_t len, struct cdc_ctx *ctx)
+{
+        size_t max_len;
+
+        crit_enter();
+        max_len = sizeof(ctx->outbuf) - ctx->out_pos;
+        if (len > max_len)
+                len = max_len;
+
+        memcpy(&ctx->outbuf[ctx->out_pos], buf, len);
+        ctx->out_pos += len;
+
+        cdc_tx_done(ctx->outbuf, -1, ctx);
+        crit_exit();
+
+        return (len);
+}
+
+
+/**
+ * control requests received:
+ * 0x21, 0x34
+ * 0x21, 0x32
+ */
+
+
+const struct usbd_function cdc_function = {
+        .interface_count = USB_FUNCTION_CDC_IFACE_COUNT,
+};
+
+void
+cdc_init(void (*data_ready_cb)(uint8_t *, size_t), void (*data_sent_cb)(size_t), struct cdc_ctx *ctx)
+{
+        usb_attach_function(&cdc_function, &ctx->header);
+        ctx->data_ready_cb = data_ready_cb;
+        ctx->data_sent_cb = data_sent_cb;
+        ctx->out_queued = 0;
+        ctx->notice_pipe = usb_init_ep(&ctx->header, CDC_NOTICE_EP, USB_EP_TX, CDC_NOTICE_SIZE);
+	ctx->tx_pipe = usb_init_ep(&ctx->header, CDC_TX_EP, USB_EP_TX, CDC_TX_SIZE);
+	ctx->rx_pipe = usb_init_ep(&ctx->header, CDC_RX_EP, USB_EP_RX, CDC_RX_SIZE);
+        cdc_rx_done(ctx->inbuf, -1, ctx);
+        cdc_tx_done(ctx->outbuf, -1, ctx);
+}
