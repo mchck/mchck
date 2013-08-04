058110e1bb1e957a6b294c8aaa1d2129375e16b3
Simon Schubert <2@0x2c.org>
2013-08-04 17:51:00 +0200

    lib/mchck: add crit_enter/exit

--- toolchain/lib/mchck/system.c
+++ toolchain/lib/mchck/system.c
@@ -9,3 +9,19 @@ sys_reset(void)
                                 }).raw;
         for (;;);
 }
+
+static int crit_nest;
+
+void
+crit_enter(void)
+{
+        __asm__("cpsid i");
+        crit_nest++;
+}
+
+void
+crit_exit(void)
+{
+        if (--crit_nest == 0)
+                __asm__("cpsie i");
+}
