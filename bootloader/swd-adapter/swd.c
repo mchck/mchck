13912595322869914c2facb9df0d59a6a65fd150
Simon Schubert <2@0x2c.org>
2013-08-10 15:59:06 +0200

    bootloader/swd-adapter: simple bitbang swd adapter code

---
+++ bootloader/swd-adapter/swd.c
@@ -0,0 +1,131 @@
+/**
+ * For now the protocol is a simple bitbang protocol.
+ *
+ * Prototcol messages:
+ * - version
+ * 0x3f...
+ * request: ?SWD?
+ * respnse: !SWD1
+ * only after this "handshake", other output functions are enabled.
+ *
+ * - write_word
+ * 0x90, 4 bytes payload (first byte, LSB transmitted first)
+ * - write_bits
+ * 0xa0 + (bitcount - 1), 1 byte payload (LSB transmitted first)
+ * - read_word
+ * 0x10 => 4 bytes
+ * - read_bits
+ * 0x20 + (bitcount - 1) => 1 byte
+ */
+
+#include <mchck.h>
+
+#include "swd.h"
+
+static enum {
+        PIN_UNCONFIGURED = 0,
+        PIN_CONFIGURED,
+} iostate = PIN_UNCONFIGURED;
+
+static const uint8_t magic[] = "?SWD?";
+static const uint8_t version[] = "!SWD1";
+
+
+static void
+write_bits(uint8_t buf, size_t bits)
+{
+        pin_configure(SWD_DIO_PIN, SWD_MODE_OUTPUT);
+        for (; bits > 0; --bits, buf >>= 1) {
+                pin_write(SWD_DIO_PIN, buf & 1);
+                pin_write(SWD_CLK_PIN, 1);
+                pin_write(SWD_CLK_PIN, 0);
+        }
+}
+
+static void
+read_bits(size_t bits)
+{
+        uint8_t val = 0;
+
+        pin_configure(SWD_DIO_PIN, SWD_MODE_INPUT);
+        for (; bits > 0; --bits) {
+                uint8_t bitval = pin_read(SWD_DIO_PIN);
+                val |= bitval << (bits - 1);
+        }
+        reply_write(&val, 1);
+}
+
+static void
+configure_pins(int on)
+{
+        pin_configure(SWD_DIO_PIN, SWD_MODE_INPUT);
+        pin_configure(SWD_CLK_PIN, on ? SWD_MODE_OUTPUT : SWD_MODE_INPUT);
+        iostate = on ? PIN_CONFIGURED : PIN_UNCONFIGURED;
+}
+
+
+const uint8_t *
+process_buf(const uint8_t *buf, size_t len)
+{
+        for (; len > 0;) {
+                uint8_t cmd = *buf;
+
+                ++buf; --len;
+
+                if (iostate == PIN_UNCONFIGURED)
+                        goto test_magic;
+
+                switch (cmd) {
+                case 0x10:       /* read word */
+                        if (reply_space() < 4) {
+                                --buf;
+                                goto need_more_data;
+                        }
+                        for (int i = 4; i > 0; --i)
+                                read_bits(8);
+                        break;
+
+                case 0x20 ... 0x28: /* read bits */
+                        if (reply_space() < 1) {
+                                --buf;
+                                goto need_more_data;
+                        }
+                        read_bits((cmd & 7) + 1);
+                        break;
+
+                case 0x90:       /* write word */
+                        if (len < 4)
+                                goto need_more_data;
+                        for (int i = 4; i > 0; --i, ++buf, --len)
+                                write_bits(*buf, 8);
+                        break;
+
+                case 0xa0 ... 0xa8: /* write bits */
+                        if (len < 1)
+                                goto need_more_data;
+                        write_bits(*buf++, (cmd & 7) + 1);
+                        break;
+
+                default:
+                case '?':        /* handshake? */
+test_magic:
+                        /* rewind what we consumed */
+                        --buf; ++len;
+                        configure_pins(0);
+                        if (len < sizeof(magic) - 1) {
+                                goto need_more_data;
+                        }
+                        if (memcmp(buf, magic, sizeof(magic) - 1) != 0)
+                                continue;
+                        /* We found a handshake! */
+                        buf += sizeof(magic) - 1;
+                        len -= sizeof(magic) - 1;
+                        configure_pins(1);
+                        reply_write(version, sizeof(version) - 1);
+                        break;
+                }
+        }
+
+need_more_data:
+        return (buf);
+}
---
+++ bootloader/swd-adapter/swd.h
@@ -0,0 +1,18 @@
+#include <mchck.h>
+
+enum swd_pin {
+        SWD_DIO_PIN = GPIO_PB16,
+        SWD_CLK_PIN = GPIO_PB3
+};
+
+enum swd_pin_mode {
+        SWD_MODE_OUTPUT = GPIO_MODE_OUTPUT,
+        SWD_MODE_INPUT = GPIO_MODE_INPUT
+};
+
+const uint8_t *process_buf(const uint8_t *buf, size_t len);
+void pin_configure(enum swd_pin pin, enum swd_pin_mode mode);
+void pin_write(enum swd_pin pin, int val);
+int pin_read(enum swd_pin pin);
+size_t reply_space(void);
+void reply_write(const uint8_t *buf, size_t len);
