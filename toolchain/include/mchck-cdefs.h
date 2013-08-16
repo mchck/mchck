eea0ab11096cbc7bed68f69bc5e96c940e44bb6f
Simon Schubert <2@0x2c.org>
2013-08-16 15:16:08 +0200

    toolchain: use _Static_assert in CTASSERT

--- toolchain/include/mchck-cdefs.h
+++ toolchain/include/mchck-cdefs.h
@@ -13,9 +13,7 @@ typedef __CHAR16_TYPE__ char16_t;
 #define __packed __attribute__((__packed__))
 
 /* From FreeBSD: compile-time asserts */
-#define CTASSERT(x)             _CTASSERT(x, __COUNTER__)
-#define _CTASSERT(x, y)         __CTASSERT(x, y)
-#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+#define CTASSERT(x)             _Static_assert(x, _STR(x))
 
 #define CTASSERT_SIZE_BYTE(t, s)     CTASSERT(sizeof(t) == (s))
 #define CTASSERT_SIZE_BIT(t, s)     CTASSERT(sizeof(t) * 8 == (s))
