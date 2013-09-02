4262049bf902b86088069462ea8e80b1cda1fad5
Simon Schubert <2@0x2c.org>
2013-09-02 23:42:15 +0200

    lib: break out pin_* from gpio_*

--- toolchain/lib/mchck/mchck.h
+++ toolchain/lib/mchck/mchck.h
@@ -1,4 +1,5 @@
 #include <mchck/system.h>
+#include <mchck/pin.h>
 #include <mchck/gpio.h>
 #include <mchck/flash.h>
 #include <mchck/onboard-led.h>
--- toolchain/lib/mchck/onboard-led.c
+++ toolchain/lib/mchck/onboard-led.c
@@ -5,7 +5,8 @@ static const enum gpio_pin_id led_pin = GPIO_PTB16;
 void
 onboard_led(enum onboard_led_state state)
 {
-        gpio_mode(led_pin, GPIO_MODE_OUTPUT | GPIO_MODE_DRIVE_HIGH);
+        gpio_dir(led_pin, GPIO_OUTPUT);
+        pin_mode(led_pin, PIN_MODE_DRIVE_HIGH);
 
         if (state == ONBOARD_LED_OFF || state == ONBOARD_LED_ON)
                 gpio_write(led_pin, state);
