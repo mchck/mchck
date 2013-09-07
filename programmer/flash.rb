3b2173b407aacb8db999f13ca4b88eb595801bb2
Simon Schubert <2@0x2c.org>
2013-09-07 13:25:39 +0200

    programmer/flash: support debug mass erase

--- programmer/flash.rb
+++ programmer/flash.rb
@@ -5,18 +5,28 @@ require 'backend-driver'
 
 $stderr.puts "Attaching debugger..."
 adiv5 = Adiv5.new(BackendDriver.from_string(ARGV[0]))
-k = Kinetis.new(adiv5, true)
-$stderr.puts "done."
 
-firmware = File.read(ARGV[1], :mode => 'rb')
-address = Integer(ARGV[2])
+if ARGV[1] == '--mass-erase'
+  k = KinetisBase.new(adiv5)
+  $stderr.puts "done."
+  $stderr.puts "Mass erasing chip..."
+  k.mass_erase
+  $stderr.puts "done."
+else
+  k = Kinetis.new(adiv5, true)
+  $stderr.puts "done."
 
-$stderr.puts "Programming %d bytes of firmware to address %#x..." % [firmware.bytesize, address]
-k.halt_core!
-k.program(address, firmware) do |address, i, total|
-  $stderr.puts "programming %#x, %d of %d" % [address, i, total]
+  firmware = File.read(ARGV[1], :mode => 'rb')
+  address = Integer(ARGV[2])
+
+  $stderr.puts "Programming %d bytes of firmware to address %#x..." % [firmware.bytesize, address]
+  k.halt_core!
+  k.program(address, firmware) do |address, i, total|
+    $stderr.puts "programming %#x, %d of %d" % [address, i, total]
+  end
+  $stderr.puts "done."
 end
-$stderr.puts "done."
+
 $stderr.puts "resetting..."
 k.reset_system!
 k.disable_debug!
