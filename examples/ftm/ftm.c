fa0513ff384344beac115440d16c697b1c375e51
Roy Smeding <roy.smeding@gmail.com>
2013-10-06 14:58:39 +0200

    examples/ftm: fix style and add comments

--- examples/ftm/ftm.c
+++ examples/ftm/ftm.c
@@ -1,21 +1,31 @@
 #include <mchck.h>
 
+/*
+ * This code will increase the PWM duty cycle from 0% to 100%, then
+ * decrease it from 100% to 0%, and so on.
+ */
 int
 main(void)
 {
-    int inc = 1;
-    long fract v = 0.0;
-    ftm_init();
+	int inc = 1;		/* Currently incrementing */
+	long fract v = 0.0;	/* Current PWM duty cycle */
+
+	/* Initialize FTM and set PWM pin mode */
+	ftm_init();
 	pin_mode(PIN_PTC1, PIN_MODE_MUX_ALT4);
 
+	/* XXX: Switch to timeouts instead of busy-wait */
 	for (;;) {
-        if(inc)
-            v += 0.00001lr;
-        else
-            v -= 0.00001lr;
+		/* Increment or decrement PWM duty slightly */
+		if (inc)
+			v += 0.00001lr;
+		else
+			v -= 0.00001lr;
 
-        if(v >= 1 || v <= 0) inc = !inc;
+		/* Switch between increment and decrement */
+		if (v >= 1 || v <= 0) inc = !inc;
 
-        ftm_set(FTM_CH0, v);
-    }
+		/* Set the actual PWM duty cycle */
+		ftm_set(FTM_CH0, v);
+	}
 }
