1ae2ab62cdc40dfd1ae4cc277bbdeadb71742bd4
Simon Schubert <2@0x2c.org>
2013-10-15 21:25:45 +0200

    Revert "examples: add broken i2c code"
    This reverts commit 67b060a2a35c46b9a43675fb8517187b25d22014.
    
    Broke blink example code.

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -1,21 +1,17 @@
 #include <mchck.h>
 
-static const enum gpio_pin_id led_pin = GPIO_PTC0;
 static struct timeout_ctx t;
 
 static void
 blink(void *data)
 {
+	onboard_led(ONBOARD_LED_TOGGLE);
 	timeout_add(&t, 500, blink, NULL);
-	gpio_toggle(led_pin);
 }
 
 int
 main(void)
 {
-	gpio_dir(led_pin, GPIO_OUTPUT);
-	pin_mode(led_pin, PIN_MODE_DRIVE_HIGH);
-
 	timeout_init();
 	/* blink will also setup a timer to itself */
 	blink(NULL);
