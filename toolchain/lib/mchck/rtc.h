a6eaabf851ba4ba75383ba4240c6db661b4dbffa
Ben Gamari <bgamari.foss@gmail.com>
2013-12-28 23:16:43 -0500

    rtc: Add some utility functions

---
+++ toolchain/lib/mchck/rtc.h
@@ -0,0 +1,19 @@
+/* Enable access to the RTC and starts the oscillator. */
+void rtc_init();
+
+/* Start the RTC counter
+ *
+ * This can fail if the counter value is invalid, as is the case
+ * after power-on. This is resolved by setting the counter value
+ * (e.g. with rtc_set_time).
+ *
+ * One should allow around one second between calling rtc_init and
+ * starting the counter to allow the oscillator to stabilize.
+ */
+int rtc_start_counter();
+
+/* Get the current time in seconds */
+uint32_t rtc_get_time();
+
+/* Set the current time in seconds */
+void rtc_set_time(uint32_t seconds);
