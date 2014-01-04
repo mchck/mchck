e6e54e864535e46948f3e9cf76794c0c5e17b9af
Brad Luyster <BradLuyster+git@gmail.com>
2014-01-04 13:24:12 -0500

    Add fflush for manual stdio flushing.
    This is useful if you want to print something like an input prompt without a newline,
    or any other use cases in which you can get short bursts of data without a newline.

--- toolchain/lib/mchck/stdio.h
+++ toolchain/lib/mchck/stdio.h
@@ -25,3 +25,4 @@ int vfprintf(FILE *f, const char *fmt, va_list args);
 void fputc(int c, FILE *f);
 int snprintf(char *buf, size_t n, const char *fmt, ...) __attribute__((__format__ (printf, 3, 4)));
 int vsnprintf(char *buf, size_t n, const char *fmt, va_list args);
+int fflush(FILE *f);
