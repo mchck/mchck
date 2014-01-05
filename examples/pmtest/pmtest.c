a2dab8041e4c8556ce0d8215f5b110bf60f8a42d
Ben Gamari <bgamari.foss@gmail.com>
2014-01-05 15:53:55 -0500

    pmtest: Modify to cycle through power modes

--- examples/pmtest/pmtest.c
+++ examples/pmtest/pmtest.c
@@ -1,10 +1,5 @@
 #include <mchck.h>
 
-static struct timeout_ctx t;
-
-volatile unsigned int llwu_count = 0;
-volatile unsigned int wakeup_count = 0;
-volatile bool state = false;
 volatile bool cont = false;
 
 static void
@@ -28,34 +23,43 @@ static void
 enter_vlpr()
 {
         // Enable very low power modes
-        move_to_blpi();
         SIM.clkdiv1.raw = ((struct SIM_CLKDIV1_t) { .outdiv1 = 1, .outdiv2 = 1, .outdiv4 = 3 }).raw;
         SMC.pmprot.raw = ((struct SMC_PMPROT) { .avlls = 1, .alls = 1, .avlp = 1 }).raw;
+
         SMC.pmctrl.raw = ((struct SMC_PMCTRL) { .runm = RUNM_VLPR, .stopm = STOPM_VLPS }).raw;
-        //SMC.pmctrl.raw = ((struct SMC_PMCTRL) { .runm = RUNM_RUN, .stopm = STOPM_VLPS }).raw;
-        SMC.pmctrl.stopm = STOPM_LLS;
-        onboard_led(1);
-        while (PMC.regsc.regons != 0);
-        while (SMC.pmstat.pmstat != PMSTAT_VLPR);
-        onboard_led(0);
+
+        if (0) {
+                onboard_led(1);
+                while (PMC.regsc.regons != 0);
+                while (SMC.pmstat.pmstat != PMSTAT_VLPR);
+                onboard_led(0);
+        }
 }
 
-struct spiflash_transaction trans;
-void powerdown_cb(void *cbdata) { }
+unsigned int blink_count = 1000000;
+void
+blink_once()
+{
+        onboard_led(1);
+        for (volatile int i=0; i<blink_count; i++);
+        onboard_led(0);
+        for (volatile int i=0; i<blink_count; i++);
+}
+
+void
+blink(unsigned int n)
+{
+        for (unsigned int i=0; i < n; i++)
+                blink_once();
+}
 
 void
 sleep(void)
 {
         __asm__("wfi");
         if (SMC.pmctrl.stopa) {
-                gpio_write(GPIO_PTA2, 1);
+                gpio_write(GPIO_PTA18, 1);
         }
-}
-
-void
-sleep_forever(void)
-{
-        for (;;) sleep();
 }
 
 void
@@ -63,46 +67,6 @@ sleep_until_cont(void)
 {
         cont = false;
         while (!cont) sleep();
-}
-
-int
-main(void)
-{
-        SIM.sopt1.usbregen = 0;
-        SCB.scr.sleepdeep = 1;
-        SMC.pmctrl.lpwui = 0;
-
-        // pull up unused SPI FLASH pins
-        pin_mode(PIN_PTC0, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
-        pin_mode(PIN_PTC5, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
-        pin_mode(PIN_PTC6, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
-        pin_mode(PIN_PTC7, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
-                
-        enter_vlpr();
-
-        if (0) {
-                ftm_init();
-                int_enable(IRQ_FTM0);
-                FTM0.sc.toie = 1;
-        }
-
-        // LED (PTA18)
-        pin_mode(PIN_PTA18, PIN_MODE_MUX_GPIO);
-        gpio_dir(GPIO_PTA18, GPIO_OUTPUT);
-
-        // STOPA output (PTA2)
-        pin_mode(PIN_PTA2, PIN_MODE_MUX_GPIO);
-        gpio_dir(GPIO_PTA2, GPIO_OUTPUT);
-
-        // button (PTA4)
-        pin_mode(PIN_PTA4, PIN_MODE_MUX_GPIO | PIN_MODE_PULLUP);
-        gpio_dir(GPIO_PTA4, GPIO_INPUT);
-        PORTA.pcr[4].irqc = PCR_IRQC_INT_FALLING;
-        int_enable(IRQ_PORTA);
-        int_enable(IRQ_LLWU);
-        LLWU.wupe[0].wupe3 = LLWU_PE_FALLING;
-
-        sleep_forever();
 }
 
 volatile unsigned int acc = 0;
@@ -118,25 +82,133 @@ void long_task(void)
 }
 
 void
+spin(void)
+{
+        cont = false;
+        while (!cont);
+}
+
+void
+set_sleepdeep(bool sleepdeep)
+{
+        SCB.scr.sleepdeep = sleepdeep;
+        volatile bool sync = SCB.scr.sleepdeep;
+}
+
+int
+main(void)
+{
+        // ensure we stay in VLPR
+        SMC.pmctrl.lpwui = 0;
+        // but we want to be able to run after be awoken
+        SCB.scr.sleeponexit = 0;
+
+        // pull up unused SPI FLASH pins
+        pin_mode(PIN_PTC0, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
+        pin_mode(PIN_PTC5, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
+        pin_mode(PIN_PTC6, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
+        pin_mode(PIN_PTC7, PIN_MODE_PULLUP | PIN_MODE_MUX_GPIO);
+
+        // setup button (PTA4)
+        pin_mode(PIN_PTA4, PIN_MODE_MUX_GPIO | PIN_MODE_PULLUP);
+        gpio_dir(GPIO_PTA4, GPIO_INPUT);
+        PORTA.pcr[4].irqc = PCR_IRQC_INT_FALLING;
+        int_enable(IRQ_PORTA);
+        LLWU.wupe[0].wupe3 = LLWU_PE_FALLING;
+        int_enable(IRQ_LLWU);
+
+        // Extra LED (PTA18)
+        pin_mode(PIN_PTA18, PIN_MODE_MUX_GPIO);
+        gpio_dir(GPIO_PTA18, GPIO_OUTPUT);
+
+        // stage 1: baseline RUN
+        blink(1);
+        spin();
+
+        // stage 2: RUN (USB regulator disabled)
+        blink(2);
+        SIM.sopt1.usbregen = 0;
+        spin();
+
+        // stage 3: wait
+        blink(3);
+        set_sleepdeep(0);
+        sleep_until_cont();
+
+        // stage 4: stop
+        blink(4);
+        set_sleepdeep(1);
+        sleep_until_cont();
+
+        // stage 5: BLPI RUN
+        blink(5);
+        move_to_blpi();
+        blink_count /= 100;
+        spin();
+
+        // stage 6: BLPI STOP
+        blink(6);
+        set_sleepdeep(1);
+        sleep_until_cont();
+
+        // stage 7: VLP RUN
+        blink(7);
+        enter_vlpr();
+        spin();
+
+        // stage 8: VLP WAIT
+        blink(8);
+        set_sleepdeep(0);
+        sleep_until_cont();
+
+        // stage 9: VLP STOP
+        blink(9);
+        set_sleepdeep(1);
+        sleep_until_cont();
+
+        // stage 10: LLS
+        blink(10);
+        SMC.pmctrl.stopm = STOPM_LLS;
+        set_sleepdeep(1);
+        sleep_until_cont();
+        
+        // stage 11: VLLS3
+        /*
+        blink(11);
+        SMC.pmctrl.stopm = STOPM_VLLS;
+        SMC.vllsctrl.vllsm = 3;
+        set_sleepdeep(1);
+        sleep_until_cont();
+        */
+
+        // For identifying clock rate
+        if (0) {
+                ftm_init();
+                int_enable(IRQ_FTM0);
+                FTM0.sc.toie = 1;
+        }
+
+        // done
+        onboard_led(1);
+        while (true)
+                for (;;) sleep();
+}
+
+void
 PORTA_Handler(void)
 {
         PORTA.isfr = 0xffffffff;
         cont = true;
-        state = !state;
-        gpio_write(GPIO_PTA18, state);
-        //onboard_led(state);
-        long_task();
+        //onboard_led(-1);
 }
 
 void
 LLWU_Handler(void)
 {
-        llwu_count++;
         LLWU.wuf1 = 0xff;
         LLWU.wuf2 = 0xff;
         LLWU.mwuf = 0xff;
-        onboard_led(-1);
-        PORTA_Handler();
+        cont = true;
 }
 
 volatile unsigned int n = 0;
