bf1fcf6b7191d244964ce586ff7ba568bc956261
Roy Smeding <roy.smeding@gmail.com>
2014-01-17 20:17:11 +0100

    toolchain: implemented corecode's i2c recommendations, fixed bugs
    * Removed references to the glitch filter
    * Changed the callback invocation
    * Got rid of the i2c.h macros; too intransparent (and would break in
        loops)
    * Replaced 'UNUSED' dummy read with a void cast, inspection of the
        generated assembly verifies that it does the right thing
    * Created enum for read/write, clearer that way
    * Invoke callback after everything else is done
    * Removed a debug statement that I missed
    * Little bit more code deduplication
    * Misc style changes

--- examples/i2c/i2c-scan.c
+++ examples/i2c/i2c-scan.c
@@ -18,62 +18,62 @@ void
 send_complete(enum i2c_result result, struct i2c_transaction *transaction)
 {
 	// print column header
-        if (probe.address == 0)
-                printf("\r\n  0123456789ABCDEF\r\n");
+	if (probe.address == 0)
+		printf("\r\n  0123456789ABCDEF\r\n");
 
 	// print row header
-        if (probe.address % 16 == 0)
-                printf("%x:", probe.address / 16);
+	if (probe.address % 16 == 0)
+		printf("%x:", probe.address / 16);
 
 	// print result
-        printf("%c", (result == I2C_RESULT_SUCCESS) ? 'X' : '.');
+	printf("%c", (result == I2C_RESULT_SUCCESS) ? 'X' : '.');
 
 	probe.address++;
-        probe.address &= 0x7f;			// Constrain the address to be between 0 and 128, 7 bits
+	probe.address &= 0x7f;			// Constrain the address to be between 0 and 128, 7 bits
 
 	// print end of line
 	if (probe.address % 16 == 0)
-                printf("\r\n");
+		printf("\r\n");
 
 	// scan next address
-        uint32_t delay = probe.address == 0 ? TIMEOUT_REPEAT : 0;
-        timeout_add(&t, delay, initiate_send, NULL);
+	uint32_t delay = probe.address == 0 ? TIMEOUT_REPEAT : 0;
+	timeout_add(&t, delay, initiate_send, NULL);
 }
 
 void
 initiate_send(void *cbdata)
 {
-        i2c_queue(&probe);
+	i2c_queue(&probe);
 }
 
 static void
 new_data(uint8_t *data, size_t len)
 {
-        cdc_read_more(&cdc);
+	cdc_read_more(&cdc);
 }
 
 void
 init_vcdc(int config)
 {
-        cdc_init(new_data, NULL, &cdc);
-        cdc_set_stdout(&cdc);
+	cdc_init(new_data, NULL, &cdc);
+	cdc_set_stdout(&cdc);
 
 	// set up I2C transaction struct
-        probe.address = 0;
-	probe.read = 0;
+	probe.address = 0;
+	probe.direction = I2C_WRITE;
 	probe.buffer = NULL;
 	probe.length = 0;
 	probe.cb = send_complete;
 	probe.cbdata = NULL;
 
-        timeout_add(&t, 10, initiate_send, NULL);
+	timeout_add(&t, 10, initiate_send, NULL);
 }
 
 void
 main(void)
 {
-        timeout_init();
-        i2c_init(I2C_RATE_100);
-        usb_init(&cdc_device);
-        sys_yield_for_frogs();
+	timeout_init();
+	i2c_init(I2C_RATE_100);
+	usb_init(&cdc_device);
+	sys_yield_for_frogs();
 }
--- toolchain/lib/mchck/i2c.h
+++ toolchain/lib/mchck/i2c.h
@@ -1,40 +1,45 @@
 enum i2c_stop {
-        I2C_NOSTOP,
-        I2C_STOP
+	I2C_NOSTOP,
+	I2C_STOP,
+};
+
+enum i2c_direction {
+	I2C_READ,
+	I2C_WRITE,
 };
 
 enum i2c_rate {
-        I2C_RATE_100 = 0,
-        I2C_RATE_400,
-        I2C_RATE_600,
-        I2C_RATE_800,
-        I2C_RATE_1000,
-        I2C_RATE_1200,
-        I2C_RATE_1500,
-        I2C_RATE_2000,
-        I2C_RATE_2400
+	I2C_RATE_100 = 0,
+	I2C_RATE_400,
+	I2C_RATE_600,
+	I2C_RATE_800,
+	I2C_RATE_1000,
+	I2C_RATE_1200,
+	I2C_RATE_1500,
+	I2C_RATE_2000,
+	I2C_RATE_2400,
 };
 
 enum i2c_result {
-        I2C_RESULT_SUCCESS,
-        I2C_RESULT_NACK
+	I2C_RESULT_SUCCESS,
+	I2C_RESULT_NACK,
 };
 
 struct i2c_transaction;
 
-typedef void (i2c_cb)(enum i2c_result result,
-		struct i2c_transaction *transaction
-	);
+typedef void	(i2c_cb)(enum i2c_result result,
+		    struct i2c_transaction *transaction
+		    );
 
 struct i2c_transaction {
-	// address & R/!W bit
-	uint8_t address : 7;
-	uint8_t read : 1;
+	// destination address and whether read or write 
+	uint8_t address;
+	enum i2c_direction direction;
 
 	// whether to end with stop condition
 	enum i2c_stop stop;
 
-	// buffer and length
+	// data buffer and length
 	uint8_t *buffer;
 	size_t length;
 
@@ -46,26 +51,6 @@ struct i2c_transaction {
 	struct i2c_transaction *next;
 };
 
-void
-i2c_init(enum i2c_rate rate);
+void	i2c_init(enum i2c_rate rate);
 
-void
-i2c_queue(struct i2c_transaction *transaction);
-
-// these are macros to get a new struct each time they're invoked
-// this means they consume some memory!
-#define _i2c_queue_read(var, addr, stop, buffer, length, cb, cbdata) {\
-		static struct i2c_transaction var = { addr, 1, stop, buffer, length, cb, cbdata, NULL };\
-		i2c_queue(&var);\
-	}
-
-#define _i2c_queue_write(var, addr, stop, buffer, length, cb, cbdata) {\
-		static struct i2c_transaction var = { addr, 0, stop, buffer, length, cb, cbdata, NULL };\
-		i2c_queue(&var);\
-	}
-
-#define i2c_queue_read(addr, stop, buffer, length, cb, cbdata) \
-	_i2c_queue_read(_CONCAT(i2c_transaction_, __COUNTER__), addr, stop, buffer, length, cb, cbdata)
-
-#define i2c_queue_write(addr, stop, buffer, length, cb, cbdata) \
-	_i2c_queue_write(_CONCAT(i2c_transaction_, __COUNTER__), addr, stop, buffer, length, cb, cbdata)
+void	i2c_queue(struct i2c_transaction *transaction);
