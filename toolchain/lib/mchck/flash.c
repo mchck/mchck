005429d10b8e755889d831b08d8169e6b6340d15
Ben Gamari <ben@smart-cactus.org>
2015-01-30 06:36:28 -0500

    flash: More useful return codes from program_sector

--- toolchain/lib/mchck/flash.c
+++ toolchain/lib/mchck/flash.c
@@ -63,12 +63,14 @@ flash_program_section(uintptr_t addr, size_t len)
 int
 flash_program_sector(const char *buf, uintptr_t addr, size_t len)
 {
+        if (len != FLASH_SECTOR_SIZE)
+                return 2;
+        if ((addr & (FLASH_SECTOR_SIZE - 1)) != 0)
+                return 3;
+        if (flash_erase_sector(addr))
+                return 4;
+
         int ret = 0;
-
-        ret = ret || (len != FLASH_SECTOR_SIZE);
-        ret = ret || ((addr & (FLASH_SECTOR_SIZE - 1)) != 0);
-        ret = ret || flash_erase_sector(addr);
-
         for (int i = FLASH_SECTOR_SIZE / FLASH_SECTION_SIZE; i > 0; --i) {
                 memcpy(flash_get_staging_area(addr, FLASH_SECTION_SIZE),
                        buf,
@@ -78,7 +80,7 @@ flash_program_sector(const char *buf, uintptr_t addr, size_t len)
                 addr += FLASH_SECTION_SIZE;
         }
 
-        return (ret);
+        return ret;
 }
 
 void *
